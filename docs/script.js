e54f26ca2f545faeae4202944566621554cb419f
clarissafurtado7 <59515708+clarissafurtado7@users.noreply.github.com>
2021-03-28 19:06:40 -0400

    Add files via upload

---
+++ docs/script.js
@@ -0,0 +1,17 @@
+/*import React from 'react';
+import ReactDOM from 'react-dom';
+
+function hoverChange(e) {
+  // Call this extremely useful function on an <img>.
+  // The <img> will become a picture of a doggy.
+  //e.target.setAttribute('src', 'https://content.codecademy.com/courses/React/react_photo-puppy.jpeg');
+  //e.target.setAttribute('alt', 'doggy');
+}
+
+const fence = (
+	<img 
+    onMouseOver={hoverChange}
+		src="https://www.publicfinance.co.uk/sites/default/files/web_jail_child_istock-838159354.png" id alt="Two small hands, belonging to a child, grip a chain-link fence. Text overlaid on the image reads 'An estimated 2.1 million children are arrested annually in the US'" />
+);
+
+ReactDOM.render(fence, document.getElementById('script'));*/
